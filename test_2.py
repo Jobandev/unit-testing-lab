9676388bbffcfbdb0565e37980aa3235fcf8c639
unknown <joban978jk@gmail.com>
2025-08-21 12:58:28 +1200

    Updated

--- test_2.py
+++ test_2.py
@@ -1,13 +1,13 @@
 from example2 import reverse_string
 
 def test_reverse_normal():
-    assert reverse_string("hello") == "olleh"
+    assert reverse_string("Joban") == "naboj"
 
 def test_reverse_empty():
     assert reverse_string("") == ""
 
 def test_reverse_palindrome():
-    assert reverse_string("madam") == "madam"
+    assert reverse_string("mall") == "llam"
 
 def test_reverse_numbers():
     assert reverse_string("12345") == "54321"
